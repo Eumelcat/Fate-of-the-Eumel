3c45c7b31b4f5bea0e14556d954513975055217d
Eumelcat <ymlctymlct@gmail.com>
2026-04-30 20:44:38 +0200

    brrrroOOOOOoOOoO Diese scheiss animationen ich hab kein bock mehr

--- Eumels FATEE/Assets/_GAME_/Player/Visuals/Scripts/Player_Selectability.cs
+++ Eumels FATEE/Assets/_GAME_/Player/Visuals/Scripts/Player_Selectability.cs
@@ -5,17 +5,30 @@ using UnityEngine.PlayerLoop;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+   #region Enums
+
+   private enum Directions { up, down, left, right }
+   #endregion
+   
+   
    #region Editor Data
    [Header("Movement Attributes")]
    [SerializeField] float _moveSpeed = 50f;
    
-   [Header("Dependencies")] [SerializeField]
-   private Rigidbody2D _rb;
+   [Header("Dependencies")] 
+   [SerializeField] Rigidbody2D _rb;
+   [SerializeField] Animator _animator;
+   [SerializeField] SpriteRenderer _spriteRenderer;
    #endregion
    
    
    #region Internal Data
    private Vector2 _moveDirection = Vector2.zero;
+   private Directions _facingDirecction = Directions.down;
+
+   private readonly int _animationMoveDown = Animator.StringToHash("Animation_YLMCT_Move_Down");
+   private readonly int _animationIdle = Animator.StringToHash("Animation_YMLCT_idle");
+   
    #endregion
 
    #region tick
@@ -23,6 +36,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
    private void Update()
    {
       GetInput();
+      CalcFacingDirection();
    }
 
    private void FixedUpdate()
@@ -37,8 +51,6 @@ public class NewMonoBehaviourScript : MonoBehaviour
    {
       _moveDirection.x = Input.GetAxisRaw("Horizontal");
       _moveDirection.y = Input.GetAxisRaw("Vertical");
-      
-      print(_moveDirection);
    }
    #endregion
 
@@ -46,7 +58,34 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
    private void MovementUpdate()
    {
-      _rb.linearVelocity = _moveDirection * (_moveSpeed * Time.fixedDeltaTime);
+      _rb.linearVelocity = _moveDirection.normalized * (_moveSpeed * Time.fixedDeltaTime);
    }
    #endregion
+
+   #region  Animation Logic
+
+   private void CalcFacingDirection()
+   {
+      if (_moveDirection.y != 0)
+      {
+         if (_moveDirection.y > 0) //Moving up
+         {
+            _facingDirecction = Directions.up;
+         }
+         else if (_moveDirection.y < 0)
+         {
+            _facingDirecction = Directions.down;
+         }
+      }
+      if (_moveDirection.magnitude > 0) // bewegung
+      {
+         _animator.CrossFade(_animationMoveDown, 0);
+      }
+      else
+      {
+         _animator.CrossFade(_animationIdle, 0);
+      }
+   }
+   #endregion
+   
 }
