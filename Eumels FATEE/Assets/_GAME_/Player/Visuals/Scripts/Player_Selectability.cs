623ea9b153e22d04ff2c30f2ad3501d691d280ad
freeeeeedyTheReal <freddoidh2o@gmail.com>
2026-05-11 16:38:37 +0200

    UPDATE
    lol, ganz viel update

--- Eumels FATEE/Assets/_GAME_/Player/Visuals/Scripts/Player_Selectability.cs
+++ Eumels FATEE/Assets/_GAME_/Player/Visuals/Scripts/Player_Selectability.cs
@@ -3,40 +3,30 @@ using UnityEngine.PlayerLoop;
 
 [SelectionBase] //allows selecting sprites
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class Player_Controller : MonoBehaviour
 {
-   #region Enums
-
-   private enum Directions { up, down, left, right }
-   #endregion
-   
-   
    #region Editor Data
+
    [Header("Movement Attributes")]
    [SerializeField] float _moveSpeed = 50f;
    
    [Header("Dependencies")] 
    [SerializeField] Rigidbody2D _rb;
    [SerializeField] Animator _animator;
-   [SerializeField] SpriteRenderer _spriteRenderer;
+   [SerializeField] SpriteRenderer _spriteRenderer; 
+  
    #endregion
-   
-   
-   #region Internal Data
-   private Vector2 _moveDirection = Vector2.zero;
-   private Directions _facingDirecction = Directions.down;
 
-   private readonly int _animationMoveDown = Animator.StringToHash("Animation_YLMCT_Move_Down");
-   private readonly int _animationIdle = Animator.StringToHash("Animation_YMLCT_idle");
-   
+   #region Internal Data
+   private Vector2 _moveDir = Vector2.zero;
+
    #endregion
 
    #region tick
 
    private void Update()
    {
-      GetInput();
-      CalcFacingDirection();
+      GatherInput();
    }
 
    private void FixedUpdate()
@@ -47,10 +37,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
    #endregion
    
    #region Input Logic
-   private void GetInput()
+   private void GatherInput()
    {
-      _moveDirection.x = Input.GetAxisRaw("Horizontal");
-      _moveDirection.y = Input.GetAxisRaw("Vertical");
+      _moveDir.x = Input.GetAxisRaw("Horizontal");
+      _moveDir.y = Input.GetAxisRaw("Vertical");
    }
    #endregion
 
@@ -58,33 +48,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
    private void MovementUpdate()
    {
-      _rb.linearVelocity = _moveDirection.normalized * (_moveSpeed * Time.fixedDeltaTime);
-   }
-   #endregion
-
-   #region  Animation Logic
-
-   private void CalcFacingDirection()
-   {
-      if (_moveDirection.y != 0)
-      {
-         if (_moveDirection.y > 0) //Moving up
-         {
-            _facingDirecction = Directions.up;
-         }
-         else if (_moveDirection.y < 0)
-         {
-            _facingDirecction = Directions.down;
-         }
-      }
-      if (_moveDirection.magnitude > 0) // bewegung
-      {
-         _animator.CrossFade(_animationMoveDown, 0);
-      }
-      else
-      {
-         _animator.CrossFade(_animationIdle, 0);
-      }
+      _rb.Velocity = _moveDir.normalized * _moveSpeed * Time.fixedDeltaTime;_
    }
    #endregion
    
